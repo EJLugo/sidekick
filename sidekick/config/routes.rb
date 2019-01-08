9b5b6cf2ec84c31ca24384e1077b3725911e51a5
Evelyn <student_104@ool-ad0202f5.dyn.optonline.net>
2019-01-07 21:42:00 -0500

    fixed nested routes

--- sidekick/config/routes.rb
+++ sidekick/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :workouts
   resources :comments
-  resources :blogs
-  resources :studios
+  resources :blogs do
+  	resources :comments
+  end
+  resources :studios do
+  	resources :workouts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
