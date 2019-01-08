0a676b682a4d57d88e54cb688c4157207b4950b3
Evelyn <student_104@ool-ad0202f5.dyn.optonline.net>
2019-01-08 09:12:11 -0500

    able to show workouts in studios

--- sidekick/client/src/components/Studios.js
+++ sidekick/client/src/components/Studios.js
@@ -6,52 +6,57 @@ class Studios extends Component {
 		super(props)
 		this.state = {
 			studios: [],
-			currentStudio: {}
+			currentStudio: null
 		}
 	}
 
 
-async componentDidMount() {
-	const studios = await getStudios();
-	this.setState({
-		studios
-	})
-}
+	async componentDidMount() {
+		const studios = await getStudios();
+		this.setState({
+			studios
+		})
+	}
 
-render() {
-	return (
-		<div>
-		{this.state.studios.map(studio => (
-			<ul>
-				<li key={studio.id}>{studio.studio_name}</li>
-				<li>{studio.studio_location}</li>
-				<li>{studio.studio_contact_info}</li>
-			</ul>
-				{
-					this.state.currentStudio.length
-					?
-					<div>
-					{this.state.currentStudio.workouts.map(workout =>(
-						<div key={workout.id}>
-							showing workout info
-
-
-
-						</div>))}
-					</div>
-					:
-					<button onClick={()=> {
-						this.setState = {
-							currentStudio: studio
+	render() {
+		return (
+			<div>
+				{this.state.studios.map(studio => (
+					<div key={studio.id}>
+						<ul>
+							<li>{studio.studio_name}</li>
+							<li>{studio.studio_location}</li>
+							<li>{studio.studio_contact_info}</li>
+						</ul>
+						{
+							this.state.currentStudio && this.state.currentStudio.id === studio.id
+								?
+								<div>
+									{this.state.currentStudio.workouts.map(workout => (
+										<div key={workout.id}>
+											<ul>
+												<li>{workout.workout_type}</li>
+												<li>{workout.workout_level}</li>
+											</ul>
+										</div>
+									))}
+									<button onClick={() => {
+									this.setState({
+										currentStudio: null
+									})
+								}}>hide workouts</button>
+								</div>
+								:
+								<button onClick={() => {
+									this.setState({
+										currentStudio: studio
+									})
+								}}>show workouts</button>
 						}
-					}}>show workouts</button>
-					}
 					</div>
-				}
-			</ul>
-		))}
-		</div>
-	);
+				))}
+			</div>
+		);
 	}
 };
 
