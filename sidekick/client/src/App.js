6b5f39279eed94eec1a0cd474d62d43e001a18eb
Evelyn <student_104@ool-ad0202f5.dyn.optonline.net>
2019-01-10 13:44:03 -0500

    hash router

--- sidekick/client/src/App.js
+++ sidekick/client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 import Home from './components/Home';
