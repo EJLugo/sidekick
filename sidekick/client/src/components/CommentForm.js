04b45bf677f15e86b4e55619a5e2b29da5a1b841
Evelyn Lugo <evelynlugo@Evelyns-MacBook-Air.local>
2019-10-15 14:21:18 -0400

    changed add comment form

--- sidekick/client/src/components/CommentForm.js
+++ sidekick/client/src/components/CommentForm.js
@@ -1,30 +1,69 @@
 import React, { Component } from 'react';
 
-function CommentForm(props) {
-	return (
-		<form onSubmit={(e) => {
-			e.preventDefault();
-			props.handleSubmit(props.blog.id);
-		}}>
-			<input
-				name='username'
-				type='text'
-				value={props.username}
-				onChange={props.handleChange}
-				placeholder='User Name'
-			>
-			</input>
-			<input
-				name='comment'
-				type='text'
-				value={props.comment}
-				onChange={props.handleChange}
-				placeholder='Enter Comment'
-			>
-			</input>
-			<button>Add Your Voice</button>
-		</form>
-	)
+
+class CommentForm extends Component() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: undefined
+		};
+
+		this.handleAddComment = this.handleAddComment.bind(this);
+	}
+
+	handleAddComment(e) {
+		e.preventDefault();
+
+		const comment = e.target.elements.comment.value.trim();
+		const error = this.props.handleAddComment(comment);
+
+		this.setState({ error })
+	}
+
+	if (!error) {
+		e.target.elements.comment.value = ''; //clear the field onAddComment
+	}
+
+	render() {
+		<div>
+			{this.state.error && <p>{this.state.error}</p>}
+			<form onSubmit={this.handleAddComment}>
+				<input type='text' name='comment' />
+				<button>Add your voice</button>
+			</form>
+		</div>
+		
+	}
 }
+
+
+
+// try changing this to be a class component? Maybe? (see above)
+// function CommentForm(props) {
+// 	return (
+// 		<form onSubmit={(e) => {
+// 			e.preventDefault();
+// 			props.handleSubmit(props.blog.id);
+// 		}}>
+// 			<input
+// 				name='username'
+// 				type='text'
+// 				value={props.username}
+// 				onChange={props.handleChange}
+// 				placeholder='User Name'
+// 			>
+// 			</input>
+// 			<input
+// 				name='comment'
+// 				type='text'
+// 				value={props.comment}
+// 				onChange={props.handleChange}
+// 				placeholder='Enter Comment'
+// 			>
+// 			</input>
+// 			<button>Add Your Voice</button>
+// 		</form>
+// 	)
+// }
 
 export default CommentForm;
